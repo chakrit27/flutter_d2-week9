fa00bba972a14a2a68f6aa6b0c69595b50b5ed70
chakrit27 <chakrit.indy2001@gmail.com>
2022-01-06 14:41:36 +0700

    Update v3

--- lib/screens/floataction_demo.dart
+++ lib/screens/floataction_demo.dart
@@ -8,23 +8,33 @@ class FloatActionDemo extends StatefulWidget {
 }
 
 class _FloatActionDemo extends State<FloatActionDemo> {
+  String $title = "FloatingActionButton";
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
-    String $title = "FloatingActionButton";
-
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text($title),
       ),
       body: Center(
-        child: Text("You have pressed the button time."),
+        child: Text("You have pressed the button $count time."),
       ),
+
       floatingActionButton: FloatingActionButton(
-        onPressed: () => print("Test"),
+        onPressed: () => setState(() {
+          count = count + 1;
+          print("$count");
+        }),
         tooltip: 'เพิ่มค่า',
         child: Icon(Icons.add),
       ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Container(height: 50.0,),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'appbar_demo.dart';
 // import 'scaffold_demo.dart';
-import 'floataction_demo.dart';
+// import 'floataction_demo.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter App Devalopment',
       debugShowCheckedModeBanner: false,
-      home: const FloatactionDemo(),
+      home: const AppBarDemo(),
       theme: ThemeData(primarySwatch: Colors.purple),
     );
   }
