5669d11457949c24b676c00017b8c347418aa9e9
chakrit27 <chakrit.indy2001@gmail.com>
2022-01-06 14:55:03 +0700

    Update v3

--- lib/screens/appbar_demo.dart
+++ lib/screens/appbar_demo.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppBarDemo extends StatefulWidget {
-  const AppBarDemo({Key? key}) :super(key: key);
+  const AppBarDemo({Key? key}) : super(key: key);
 
   @override
   State<AppBarDemo> createState() => _AppBarDemoState();
@@ -17,19 +17,35 @@ class _AppBarDemoState extends State<AppBarDemo> {
         title: Text("Welcome to Flutter"),
         actions: [
           IconButton(
-              onPressed: (){
+              onPressed: () {
                 setState(() {
                   count = count + 1;
                 });
               },
               icon: Icon(Icons.add_alert)),
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  count = count - 1;
+                  print('$count');
+                });
+              },
+              icon: Icon(Icons.delete_forever_sharp)),
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  count = 0;
+                  print('$count');
+                });
+              },
+              icon: Icon(Icons.ac_unit)),
         ],
       ),
       body: Center(
         child: Text(
-          "You have pressed the button $count time.",
-          style: TextStyle(fontSize: 20.0,color: Colors.purple),
-        )
+          "$count",
+          style: TextStyle(fontSize: 50.0, color: Colors.purple),
+        ),
       ),
     );
   }
